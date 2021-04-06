1169111807b5453cb1489c337e3529b1a4f12948
JinHyeonGi <wlsgusdl119@gmail.com>
2021-04-06 17:41:11 +0900

    [UPDATE] EventController.java안에 eventPage, eventDetail 메서드 추가 & main_desktop_header.jsp안에 eventPage경로 추가

--- src/main/java/com/bkc/subpages/controller/EventController.java
+++ src/main/java/com/bkc/subpages/controller/EventController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/event", method = RequestMethod.GET)
 public class EventController {
-	
+
+	@RequestMapping(value = "/eventPage.do", method = RequestMethod.GET)
+	public String eventPage() {
+		return "subpages/event/eventPage";
+	}
+
+	@RequestMapping(value = "/eventDetail.do", method = RequestMethod.GET)
+	public String eventDetail() {
+		return "subpages/event/eventDetail";
+	}
 
 }
