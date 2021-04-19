d5eeb2b38d72d5a1a2bf4e82626c4df6c2cceae7
yoojinwon <hilu12311@gmail.com>
2021-04-19 14:36:00 +0900

    [ADD] FAQ관련 기능 추가

---
+++ src/main/java/com/bkc/customerService/dao/FaqDAO.java
@@ -0,0 +1,15 @@
+package com.bkc.customerService.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.bkc.customerService.vo.FaqVO;
+
+@Repository
+public interface FaqDAO {
+	//게시판 리스트
+	public List<FaqVO> FaqList(FaqVO faqVO);
+	
+	public int FaqTotalCnt(FaqVO faqVO);
+}
---
+++ src/main/java/com/bkc/customerService/service/FaqService.java
@@ -0,0 +1,12 @@
+package com.bkc.customerService.service;
+
+import java.util.List;
+
+import com.bkc.customerService.vo.FaqVO;
+
+public interface FaqService {
+	
+	public List<FaqVO> FaqList(FaqVO faqVO);
+	
+	public int FaqTotalCnt(FaqVO faqVO);
+}
