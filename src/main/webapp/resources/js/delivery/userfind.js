75369246cef224b3110dfe224cd21474169949bd
choijaeho <dhqkgbs@naver.com>
2021-04-21 17:56:16 +0900

    [UPDATE] 정규표현식 수정

--- src/main/webapp/resources/js/delivery/userfind.js
+++ src/main/webapp/resources/js/delivery/userfind.js
@@ -52,7 +52,6 @@ $(document).ready(function() {
 	
 	$("#st02").keyup(function() {
 		var content2 = document.getElementById("st02");
-		
 		//이메일 정규표현식 검증 
 		var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 		if(content2.value.match(regExp) != null){
@@ -60,6 +59,7 @@ $(document).ready(function() {
     		phone2.style.display = "none";
 		}
 	});
+	
 });$("#item1").click(function() {
 	$(this).addClass('on').siblings().removeClass('on');
 	info1.style.display = "block";
@@ -106,8 +106,8 @@ $("#item2").click(function() {
 });
 
 $('#st01').keyup(function(e) {
-	var content1 = $(this).val();
-	if (content1.length != 0) {
+	var content1 = document.getElementById("st01");
+	if (content1.value.length > 0) {
 		name1.style.display = "none";
 	} else {
 		name1.style.display = "block";
