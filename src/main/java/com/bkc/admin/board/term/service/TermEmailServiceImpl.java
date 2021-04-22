59c0197ec28c9f91d9c663da046d86f8e806357e
KyungMin_ <tommykmin@naver.com>
2021-04-22 14:28:09 +0900

    [ADD] 관리자 이메일약관 페이지 완료

--- src/main/java/com/bkc/admin/board/term/service/TermEmailServiceImpl.java
+++ src/main/java/com/bkc/admin/board/term/service/TermEmailServiceImpl.java
@@ -11,9 +11,22 @@ public class TermEmailServiceImpl implements TermEmailService {
 	@Autowired
 	private TermEmailDAO termEmailDao;
 	
+	//유저 이메일약관 게시글 조회
 	@Override
 	public TermEmailVO getTermEmailList() {
 		return termEmailDao.getTermEmailList();
+	}
+
+	//관리자 이메일약관 게시글 조회
+	@Override
+	public TermEmailVO getTermEmailList(TermEmailVO termEmail) {		
+		return termEmailDao.getTermEmailList(termEmail);
+	}
+
+	//관리자 이메일약관 게시글 수정
+	@Override
+	public void updateTermEmail(TermEmailVO termEmail) {
+		termEmailDao.updateTermEmail(termEmail);
 	}
 
 }
