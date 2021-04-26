13040dfc59854f247f1b7f5fa687f3829c1db7f6
choijaeho <dhqkgbs@naver.com>
2021-04-26 17:44:10 +0900

    [UPDATE] 유튜브광고 관리자쪽 완성

--- src/main/java/com/bkc/youtube/controller/YoutubeController.java
+++ src/main/java/com/bkc/youtube/controller/YoutubeController.java
@@ -122,7 +122,12 @@ public class YoutubeController {
 			}
 
 			// 2.2.2) 가져온 값을 제외한 모든 값은 미사용중으로 변경
-			ytService.changeAllStatusYoutube(img_seq);
+			if (ytService.changeAllStatusYoutube(img_seq) == 1) {
+				System.out.println("기존에 사용중인 것  변경 성공");
+			} else {
+				System.out.println("기존에 사용중인 것 변경 실패");
+			}
+			
 		}
 		return "redirect:/admin/youtubeList.ad";
 	}
