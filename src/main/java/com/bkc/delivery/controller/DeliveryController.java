fb0ac991dbf7fb1f790e7c1d01dfc9009fe42aa9
KyungMin_ <tommykmin@naver.com>
2021-04-27 10:21:53 +0900

    [UPDATE] 회원주문페이지 푸터 DB에서 받아올 수 있게 추가

--- src/main/java/com/bkc/delivery/controller/DeliveryController.java
+++ src/main/java/com/bkc/delivery/controller/DeliveryController.java
@@ -1,16 +1,29 @@
 package com.bkc.delivery.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bkc.admin.board.businessInformation.service.BusinessInformationService;
+import com.bkc.admin.board.businessInformation.vo.BusinessInformationVO;
 
 @Controller
 @RequestMapping(value = "/delivery", method = RequestMethod.GET)
 public class DeliveryController {
 	
+	@Autowired
+	private BusinessInformationService biService;
+	
+	//회원 주문 페이지로 이동
 	@RequestMapping(value = "/delivery.do", method = RequestMethod.GET)
-	public String delivery() {
+	public String delivery(Model model) {
 		System.out.println("delivery 페이지 이동");
+		
+		//푸터추가
+		BusinessInformationVO bi = biService.getBusinessInformation(1);
+		model.addAttribute("bi", bi);
 		return "delivery/delivery";
 	}
 	
