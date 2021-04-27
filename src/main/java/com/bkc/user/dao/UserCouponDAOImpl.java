cdd7cc0974f2200c93418d3171c72fd5ee4ca825
choijaeho <dhqkgbs@naver.com>
2021-04-28 00:03:09 +0900

    [UPDATE] 쿠폰 관련작업 수행중

--- src/main/java/com/bkc/user/dao/UserCouponDAOImpl.java
+++ src/main/java/com/bkc/user/dao/UserCouponDAOImpl.java
@@ -1,5 +1,27 @@
 package com.bkc.user.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bkc.user.vo.CouponVO;
+
+@Repository
 public class UserCouponDAOImpl implements UserCouponDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List<CouponVO> getCouponListById(String userid) {
+		return sqlSession.selectList("getCouponListById", userid);
+	}
+
+	@Override
+	public List<CouponVO> getCouponList() {
+		return sqlSession.selectList("getCouponList");
+	}
 
 }
--- src/main/java/com/bkc/user/service/UserCouponService.java
+++ src/main/java/com/bkc/user/service/UserCouponService.java
@@ -1,6 +1,17 @@
 package com.bkc.user.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bkc.user.vo.CouponVO;
+
+@Service
 public interface UserCouponService {
 
-	public List 
+	//쿠폰 리스트 받아오기 
+	public List<CouponVO> getCouponListById(String userid);
+
+	//모든 쿠폰리스트 받아오기 
+	public List<CouponVO> getCouponList();
 }
